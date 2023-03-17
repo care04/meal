75bc9830109a9301a52a5a6420708a40a30a1835
care04 <carefarrar@gmail.com>
2023-03-17 14:50:03 -0400

    more styling in add recipe view

--- src/views/AddRecipeView.vue
+++ src/views/AddRecipeView.vue
@@ -65,33 +65,97 @@ function deleteIngredient(ingredient: Ingredient) {
 <template>
   <Form :validation-schema="schema" @submit="save">
     <h3>New Recipe</h3>
-    <label>Name</label><br />
-    <Field name="name" v-model="recipeStore.recipe.name" /><br />
-    <ErrorMessage name="name" /><br />
-    <label>Serving Size</label><br />
-    <Field name="serving" v-model="recipeStore.recipe.serving" /><br />
-    <ErrorMessage name="serving" /><br />
-    <label>Description</label><br />
-    <Field name="description" v-model="recipeStore.recipe.description" /><br />
-    <ErrorMessage name="description" /><br />
-    <label>ingredients</label
-    ><button @click="adding = true" type="button">+</button><br />
+    <div class="form-floating">
+      <Field
+        name="name"
+        class="form-control"
+        id="name"
+        placeholder="name"
+        v-model="recipeStore.recipe.name"
+      />
+      <label for="name">Name</label>
+      <ErrorMessage name="name" />
+    </div>
+    <div class="form-floating">
+      <Field
+        name="serving"
+        class="form-control"
+        id="serving"
+        placeholder="serving"
+        v-model="recipeStore.recipe.serving"
+      />
+      <label for="serving">Serving Size</label>
+      <ErrorMessage name="serving" />
+    </div>
+    <div class="form-floating">
+      <Field
+        name="description"
+        class="form-control"
+        id="description"
+        placeholder="description"
+        v-model="recipeStore.recipe.description"
+      />
+      <label for="description">Description</label>
+      <ErrorMessage name="description" />
+    </div>
+    <div class="mb-3">
+      <label>ingredients
+      <button @click="adding = true" type="button" class="btn btn-outline-info btn-sm">+</button></label
+      >
+    </div>
     <li v-for="ingredient in recipeStore.ingredients" :key="ingredient.name">
       <button @click="deleteIngredient(ingredient)" type="button">delete</button
       >{{ ingredient.unit }} {{ "   " }} {{ ingredient.name }}
-    </li><br />
+    </li>
     <div v-if="adding === true">
       <div v-if="ingredientError != ''">{{ ingredientError }}</div>
-      <label>name</label><br />
-      <input type="text" v-model="ingredient.name" /><br />
-      <label>unit</label><br />
-      <input type="text" v-model="ingredient.unit" /><br />
-      <button @click="cancel()" type="button">cancel</button
-      ><button @click="checkIngredient()" type="button">Add ingredient</button><br />
+      <div class="form-floating">
+        <input
+          type="text"
+          v-model="ingredient.name"
+          class="form-control"
+          id="ingredient_name"
+          placeholder="Ingredient Name"
+        />
+        <label for="ingredient_name">Ingredient Name</label>
+      </div>
+      <div class="form-floating">
+        <input
+          type="text"
+          v-model="ingredient.unit"
+          class="form-control"
+          id="unit"
+          placeholder="unit"
+        />
+        <label for="unit">unit</label>
+      </div>
+      <div class="mb-3">
+        <button @click="cancel()" type="button" class="btn">cancel</button
+        ><button @click="checkIngredient()" type="button" class="btn">Add ingredient</button>
+      </div>
     </div>
-    <label>Instructions </label><br />
-    <Field name="instructions" v-model="recipeStore.recipe.instructions" /><br />
-    <ErrorMessage name="instructions" /><br />
+    <div class="form-floating">
+      <Field
+        name="instructions"
+        v-model="recipeStore.recipe.instructions"
+        class="form-control"
+        id="instructions"
+        placeholder="instructions"
+        />
+      <label for="instructions">Instructions</label>
+      <ErrorMessage name="instructions" />
+    </div>
     <button type="submit">Save Recipe</button>
   </Form>
 </template>
+<style scoped>
+.form-floating>label {
+  color: #888;
+}
+.form-floating {
+  margin-bottom: 0.5cm;
+}
+.mb-3>button {
+  color:rgb(233, 240, 246)
+}
+</style>
